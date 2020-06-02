6a1271f12a5b40832b6edc985f0c2a8e19321dec
Sebastian Krajewski <mail@wieku.me>
2020-06-02 07:29:10 +0200

    Add soil empty state

---
+++ Engine/Interactions/Custom/CarrotStates/CarrotStateGrowing.cs
@@ -0,0 +1,14 @@
+﻿namespace Game.Engine.Interactions.Custom.CarrotStates
+{
+    class CarrotStateGrowing : CarrotState
+    {
+        public CarrotStateGrowing(CarrotInteraction carrot, GameSession session) : base(carrot, session)
+        {
+        }
+
+        public override bool Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Engine/Interactions/Custom/CarrotStates/CarrotStateSoilEmpty.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Engine.Interactions.Custom.CarrotStates
+{
+    class CarrotStateSoilEmpty: CarrotState
+    {
+        public CarrotStateSoilEmpty(CarrotInteraction carrot, GameSession session) : base(carrot, session)
+        {
+        }
+
+        public override bool Execute()
+        {
+            session.SendText("This soil is empty.");
+
+            var result = session.TestForItemInInventory("item2165");
+
+            if (!result) return false;
+            
+            session.SendText("It seems you have a spare carrot, do you want to plant it?");
+            
+            var choice = session.ListBoxInteractionChoice(new List<string>() { "Sure!", "My presiousssss..." });
+            switch (choice)
+            {
+                case 0:
+                    session.RemoveItemFromInventory("item2165");
+                    carrot.carrotState = new CarrotStateGrowing(carrot, session);
+                    return true;
+                default:
+                    session.SendText("You walk away allowing carrots in your inventory to rot...");
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
