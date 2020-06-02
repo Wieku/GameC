82df5b71ef21dc657405ebdb5cbc001bb3394aae
Sebastian Krajewski <mail@wieku.me>
2020-06-02 06:53:25 +0200

    Add carrot depleted soil state

---
+++ Engine/Interactions/Custom/CarrotStates/CarrotStateSoilDepleted.cs
@@ -0,0 +1,16 @@
+﻿namespace Game.Engine.Interactions.Custom.CarrotStates
+{
+    class CarrotStateSoilDepleted: CarrotState
+    {
+        public CarrotStateSoilDepleted(CarrotInteraction carrot, GameSession session) : base(carrot, session)
+        {
+        }
+
+        public override bool Execute()
+        {
+            session.SendText("Sorry, but soil is depleted, you cannot plant carrots here anymore.");
+
+            return false;
+        }
+    }
+}
