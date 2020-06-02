1ae473c2abade16964e7fd97f560d803a3efd30e
Sebastian Krajewski <mail@wieku.me>
2020-06-02 06:56:32 +0200

    Add missing CarrotInteraction class

---
+++ Engine/Interactions/Custom/CarrotInteraction.cs
@@ -0,0 +1,23 @@
+﻿using Game.Engine.Interactions.Custom.CarrotStates;
+
+namespace Game.Engine.Interactions.Custom
+{
+    class CarrotInteraction: ListBoxInteraction
+    {
+        protected const int SoilDurability = 3;
+
+        private ICarrotState carrotState;
+
+        protected int timesGrown = 1;
+        
+        public CarrotInteraction(GameSession ses) : base(ses)
+        {
+            
+        }
+
+        protected override void RunContent()
+        {
+            while (carrotState.Execute());
+        }
+    }
+}
