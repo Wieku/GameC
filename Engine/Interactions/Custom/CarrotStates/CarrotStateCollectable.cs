72b054abb528155747e4f7d424739066cce76cb2
Sebastian Krajewski <mail@wieku.me>
2020-06-08 19:44:56 +0200

    Add random rat generation

--- Engine/Interactions/Custom/CarrotStates/CarrotStateCollectable.cs
+++ Engine/Interactions/Custom/CarrotStates/CarrotStateCollectable.cs
@@ -1,4 +1,7 @@
-﻿namespace Game.Engine.Interactions.Custom.CarrotStates
+﻿using Game.Engine.Items.Custom;
+using Game.Engine.Monsters;
+
+namespace Game.Engine.Interactions.Custom.CarrotStates
 {
     abstract class CarrotStateCollectable : CarrotState
     {
@@ -8,7 +11,18 @@
 
         protected void Collect()
         {
+            session.SendText("You collected some carrots!");
+            session.AddThisItem(new Carrot());
+            session.AddThisItem(new Carrot());
             
+            var randomRat = Index.RNG(0, 2) == 0;
+
+            if (randomRat)
+            {
+                session.SendText("Unfortunately under the carrot there was a tunnel occupied by rat and it got angry!");
+                session.FightThisMonster(new Rat(session.currentPlayer.Level));
+            }
+
         }
 
     }
